c6c6eb4e2b23b2c3724b05818fbef1fe0f8e5d91
Nguyen Truyen Tuan <tuan160404@gmail.com>
2024-12-04 13:38:40 +0700

    1

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,20 +1,21 @@
-import React from "react";
-import {Text, Box} from "@chakra-ui/react"
+import React from 'react'
+import { Text, Box } from '@chakra-ui/react'
 
 const Footer = () => {
-    return (
-        <Box 
-        display="flex" 
-        justifyContent="center" 
-        alignItems="center" 
-        height="10vh" 
-        bg="#1488D8"
-        fontFamily="Coiny">
-            <Text textAlign="center" fontSize="2xl">
-                Sản phẩm thuộc về nhóm TN01 - 06
-            </Text>
-        </Box>
-    );
-};
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="7vh"
+      bg="#1488D8"
+      fontFamily="Coiny"
+    >
+      <Text textAlign="center" fontSize="2xl">
+        Sản phẩm thuộc về nhóm TN01 - 06
+      </Text>
+    </Box>
+  )
+}
 
-export default Footer;
+export default Footer
--- frontend/src/components/MainLayout.js
+++ frontend/src/components/MainLayout.js
@@ -1,32 +1,32 @@
-import React from "react"
-import Header from "./Header"
-import SideBar from "./SideBar"
-import Footer from "./Footer"
-import {Flex, Box } from "@chakra-ui/react"
+import React from 'react'
+import Header from './Header'
+import SideBar from './SideBar'
+import Footer from './Footer'
+import { Flex, Box } from '@chakra-ui/react'
 
 /* Layout contains header and footer AND sidebar */
 const MainLayout = (props) => {
-    return (
-        <Flex direction="column" height="100vh">
-            <Box as="footer">
-                <Header/>
-            </Box>
-            <Flex flex="1" bg="white">
-                {/* SideBar */}
-                <Box as="aside">
-                    <SideBar section={props.section}/>
-                </Box>
-                {/* Main Content */}
-                <Box as="main" flex="1">
-                    {props.children}
-                </Box>
-            </Flex>
-            {/* Footer */}
-            <Box as="footer">
-                <Footer/>
-            </Box>
-        </Flex>
-        );
-};
+  return (
+    <Flex direction="column" height="100vh">
+      <Box as="footer">
+        <Header />
+      </Box>
+      <Flex flex="1" bg="white">
+        {/* SideBar */}
+        <Box as="aside">
+          <SideBar section={props.section} />
+        </Box>
+        {/* Main Content */}
+        <Box as="main" flex="1">
+          {props.children}
+        </Box>
+      </Flex>
+      {/* Footer */}
+      <Box as="footer">
+        <Footer />
+      </Box>
+    </Flex>
+  )
+}
 
-export default MainLayout;
+export default MainLayout
